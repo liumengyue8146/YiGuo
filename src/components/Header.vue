dd19bd2e11b73fda1ba7c4b17e1f9d350a492f0e
handsome <handsome19970114@163.com>
2019-09-25 22:27:44 +0800

    静态页面的搭建九月25晚上22:30

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="topSearch">
+    <van-search v-model="value" placeholder="请输入搜索关键词" shape="round"></van-search>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      value: ""
+    };
+  }
+};
+</script>
+<style scoped>
+</style>>
