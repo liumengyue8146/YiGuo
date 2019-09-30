9cd919cd71cd16ea8be858af81610c235ff6bbbe
liumengyue <18838460801@163.com>
2019-09-30 16:07:36 +0800

    优化头部

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,16 @@
-<<<<<<< HEAD
-const autoprefixer = require('autoprefixer');
-const pxtorem = require('postcss-pxtorem');
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-  },
-  plugins: {
-    autoprefixer: {
-      // browsers: ['Android >= 4.0', 'iOS >= 7']
-    },
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*'],
-    },
-  },
-};
-=======
 // const autoprefixer = require('autoprefixer');
 // const pxtorem = require('postcss-pxtorem');
 // module.exports = {
-//     plugins: {
-//         autoprefixer: {},
+//   plugins: {
+//     autoprefixer: {},
+//   },
+//   plugins: {
+//     autoprefixer: {
+//       // browsers: ['Android >= 4.0', 'iOS >= 7']
 //     },
-//     plugins: {
-//         'autoprefixer': {
-//             browsers: ['Android >= 4.0', 'iOS >= 7']
-//         },
-//         'postcss-pxtorem': {
-//             rootValue: 37.5,
-//             propList: ['*']
-//         }
-//     }
-
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*'],
+//     },
+//   },
 // };
->>>>>>> f3076a49f8f38f7796035e62107fd3f745ec97ed
